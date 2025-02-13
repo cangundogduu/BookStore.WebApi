b2e0096b55fa14f9d33b3b9cde6bc4c983cdda4e
can gündoğdu <gundogdu.can@hotmail.com>
2025-02-13 21:54:51 +0300

    13.02.2025

---
+++ BookStore.WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,65 @@
+﻿using BookStore.BusinessLayer.Abstract;
+using BookStore.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+
+        [HttpGet]
+        public IActionResult ProductList()
+        {
+            var value = _productService.TGetAll();
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public IActionResult CreateProduct(Product product)
+        {
+            _productService.TAdd(product);
+            return Ok("Ekleme işlemi başarı ile gerçekleştirildi.");
+        }
+
+
+        [HttpPut]
+        public IActionResult UpdateProduct(Product product)
+        {
+            _productService.TUpdate(product);
+            return Ok("Güncelleme işlemi başarı ile gerçekleştirildi.");
+        }
+
+
+        [HttpDelete]
+        public IActionResult DeleteProduct(int id)
+        {
+            _productService.TDelete(id);
+            return Ok("Silme işlemi başarı ile gerçekleştirildi.");
+        }
+
+        [HttpGet("GetProduct")]
+        public IActionResult GetProduct(int id)
+        {
+            var value = _productService.TGetById(id);
+            return Ok(value);
+        }
+
+        [HttpGet("GetProductCount")]
+        public IActionResult GetProductCount()
+        {
+            var value = _productService.TGetProductCount();
+            return Ok(value);
+        }
+
+    }
+}
