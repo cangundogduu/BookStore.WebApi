1070c58ef48c16c12d05155321c6afa1d8cebbbd
can gündoğdu <gundogdu.can@hotmail.com>
2025-02-20 22:05:43 +0300

    20.02.2025

--- BookStore.WebUl/Controllers/CategoryController.cs
+++ BookStore.WebUl/Controllers/CategoryController.cs
@@ -1,13 +1,63 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookStore.WebUI.Dtos.CategoryDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace BookStore.WebUI.Controllers
 {
     public class CategoryController : Controller
     {
 
+        private readonly IHttpClientFactory _httpCilentFactory;
+
+        public CategoryController(IHttpClientFactory httpClientFactory)
+        {
+            _httpCilentFactory = httpClientFactory;
+        }
+
+
         public async Task<IActionResult> CategoryList()
         {
+
+            var client = _httpCilentFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7228/api/Categories");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+                return View(values);
+
+            }
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryDto createCategoryAddDto)
+        {
+            var client = _httpCilentFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createCategoryAddDto);
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7228/api/Categories", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("CategoryList");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> DeleteCategory(int id)
+        {
+            var client = _httpCilentFactory.CreateClient();
+            await client.DeleteAsync("https://localhost:7228/api/Categories?id=" + id);
+            return RedirectToAction("CategoryList");
+
+
         }
     }
 }
---
+++ BookStore.WebUl/Controllers/ProductController.cs
@@ -0,0 +1,86 @@
+﻿using BookStore.WebUI.Dtos.ProductDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace BookStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IHttpClientFactory _httpCilentFactory;
+
+        public ProductController(IHttpClientFactory httpCilentFactory)
+        {
+            _httpCilentFactory = httpCilentFactory;
+        }
+
+        public async Task<IActionResult> ProductList()
+        {
+            var client = _httpCilentFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7228/api/Products");
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult CreateProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct(CreateProductDto createProductDto)
+        {
+            var client = _httpCilentFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createProductDto);
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7228/api/Products", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("ProductList");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            var client = _httpCilentFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync("https://localhost:7228/api/Products?id=" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("ProductList");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateProduct(int id)
+        {
+            var client = _httpCilentFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7228/api/Products/GetProduct?id=" + id);
+            if (responseMessage.IsSuccessStatusCode) 
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<GetByIdProductDto>(jsonData);
+                return View(values);
+            }
+            
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateProduct(UpdateProductDto updateProductDto)
+        {
+            var client = _httpCilentFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateProductDto);
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7228/api/Products", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("ProductList");
+            }
+            return View();
+        }
+    }
+}
