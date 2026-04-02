e4b0dfbb6a9f363284d41bc295107d40759ec12d
can gündoğdu <gundogdu.can@hotmail.com>
2026-04-02 22:45:55 +0300

    02.04.2026

--- BookStore.WebUl/Controllers/QuoteController.cs
+++ BookStore.WebUl/Controllers/QuoteController.cs
@@ -1,6 +1,8 @@
 ﻿using BookStore.WebUI.Dtos.QuoteDto;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Text;
+using System.Text.Json.Serialization;
 
 namespace BookStore.WebUI.Controllers
 {
@@ -14,7 +16,67 @@ namespace BookStore.WebUI.Controllers
             {
                 var jsonData = await responsemessage.Content.ReadAsStringAsync();
                 var values = JsonConvert.DeserializeObject<List<ResultQuoteDto>>(jsonData);
-                return Ok(values);
+                return View(values);
+            }
+            return View(new List<ResultQuoteDto>());
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CreateQuote()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateQuote(CreateQuoteDto quoteDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(quoteDto);
+            var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7228/api/Quotes", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction(nameof(QuoteList));
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateQuote(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7228/api/Quotes/GetByIdCategory?id=" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<GetByIdQuoteDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateQuote(UpdateQuoteDto quoteDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var JsonData = JsonConvert.SerializeObject(quoteDto);
+            var content = new StringContent(JsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7228/api/Quotes", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction(nameof(QuoteList));
+
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> DeleteQuote(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync("https://localhost:7228/api/Quotes?id=" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction(nameof(QuoteList));
             }
             return View();
         }
